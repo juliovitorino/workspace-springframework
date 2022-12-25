15d0fd04abc7d45a9ef5b694a48b0f9972ca601d
JulioVitorino <julio.vitorino@gmail.com>
2022-12-25 19:52:41 -0300

    Spring versão 2.7.4

---
+++ microserviceb/src/main/java/br/com/jcv/microservice/microserviceb/MicroserviceBApplication.java
@@ -0,0 +1,13 @@
+package br.com.jcv.microservice.microserviceb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceBApplication.class, args);
+	}
+
+}
---
+++ microserviceb/src/test/java/br/com/jcv/microservice/microserviceb/MicroservicebApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jcv.microservice.microserviceb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
