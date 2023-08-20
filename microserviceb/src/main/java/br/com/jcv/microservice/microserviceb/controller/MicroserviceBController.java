4b1aed8f660637bfc484a2de82bd40f96849ba98
juliovitorino <julio.vitorino@gmail.com>
2023-08-20 11:22:18 -0300

    estudo do docker no microserviço b

--- microserviceb/src/main/java/br/com/jcv/microservice/microserviceb/controller/MicroserviceBController.java
+++ microserviceb/src/main/java/br/com/jcv/microservice/microserviceb/controller/MicroserviceBController.java
@@ -1,16 +1,16 @@
-package br.com.jcv.microservice.microservicea.controller;
+package br.com.jcv.microservice.microserviceb.controller;
 
-import org.apache.coyote.Response;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/v1/microservicea")
-public class MicroserviceAController {
+@RequestMapping("/v1/microserviceb")
+public class MicroserviceBController {
+
     @GetMapping("/welcome")
     public ResponseEntity<String> getMessage() {
-        return ResponseEntity.ok("Welcome to microservice A");
+        return ResponseEntity.ok("Welcome to microservice B");
     }
 }
