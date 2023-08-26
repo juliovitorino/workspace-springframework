1e13dfe9e7890f8a02120f42b5396d2c6d4ad98f
juliovitorino <julio.vitorino@gmail.com>
2023-08-26 11:00:31 -0300

    criação de estrutura jcv sprint boot projects

---
+++ jcv-environment-config/src/main/java/br/com/jcv/environmentconfig/JcvEnvironmentConfigApplication.java
@@ -0,0 +1,13 @@
+package br.com.jcv.environmentconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JcvEnvironmentConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JcvEnvironmentConfigApplication.class, args);
+	}
+
+}
---
+++ jcv-environment-config/src/test/java/br/com/jcv/environmentconfig/JcvEnvironmentConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jcv.environmentconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JcvEnvironmentConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
