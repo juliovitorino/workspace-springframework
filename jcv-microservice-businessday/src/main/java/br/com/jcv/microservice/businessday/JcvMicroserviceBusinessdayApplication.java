5cb5c6b1538b5f8db996babc93dcbacaa333f22f
juliovitorino <julio.vitorino@gmail.com>
2023-08-27 08:07:00 -0300

    Estrutura de serviço para international business day

---
+++ jcv-microservice-businessday/src/main/java/br/com/jcv/microservice/businessday/JcvMicroserviceBusinessdayApplication.java
@@ -0,0 +1,13 @@
+package br.com.jcv.microservice.businessday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JcvMicroserviceBusinessdayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JcvMicroserviceBusinessdayApplication.class, args);
+	}
+
+}
---
+++ jcv-microservice-businessday/src/test/java/br/com/jcv/microservice/businessday/JcvMicroserviceBusinessdayApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jcv.microservice.businessday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JcvMicroserviceBusinessdayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
