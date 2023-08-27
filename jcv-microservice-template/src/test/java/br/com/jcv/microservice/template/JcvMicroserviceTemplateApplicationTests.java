34dbd1fce97c24e3fe153d00d938234eb055a2bd
juliovitorino <julio.vitorino@gmail.com>
2023-08-27 08:15:57 -0300

    Estrutura para microserviço de templates

---
+++ jcv-microservice-template/src/test/java/br/com/jcv/microservice/template/JcvMicroserviceTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jcv.microservice.template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JcvMicroserviceTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
