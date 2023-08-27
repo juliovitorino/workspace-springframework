9a20b743108d22982763232318bc8295cfad28ed
juliovitorino <julio.vitorino@gmail.com>
2023-08-27 08:26:05 -0300

    Estrutura para microserviço de invoice manager

---
+++ jcv-microservice-invoice-manager/src/main/java/br/com/jcv/microservice/invoice/manager/JcvMicroserviceInvoiceManagerApplication.java
@@ -0,0 +1,13 @@
+package br.com.jcv.microservice.invoice.manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JcvMicroserviceInvoiceManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JcvMicroserviceInvoiceManagerApplication.class, args);
+	}
+
+}
---
+++ jcv-microservice-invoice-manager/src/test/java/br/com/jcv/microservice/invoice/manager/JcvMicroserviceInvoiceManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jcv.microservice.invoice.manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JcvMicroserviceInvoiceManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
