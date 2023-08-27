b2670d58ec73c10b0946102a4d00cb9ebc7164e2
juliovitorino <julio.vitorino@gmail.com>
2023-08-27 08:11:27 -0300

    Estrutura para microserviço mail sender

---
+++ jcv-microservice-mailsender/src/main/java/br/com/jcv/microservice/mailsender/JcvMicroserviceMailsenderApplication.java
@@ -0,0 +1,13 @@
+package br.com.jcv.microservice.mailsender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JcvMicroserviceMailsenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JcvMicroserviceMailsenderApplication.class, args);
+	}
+
+}
---
+++ jcv-microservice-mailsender/src/test/java/br/com/jcv/microservice/mailsender/JcvMicroserviceMailsenderApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jcv.microservice.mailsender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JcvMicroserviceMailsenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
