7878e5fa10b0558f4e41ff82f8344cb9e93cd166
juliovitorino <julio.vitorino@gmail.com>
2023-09-08 09:40:02 -0300

    Criacao dos loadbalancer e gateway para microservices projeto MS. Adicao de banners.

---
+++ ms-cloud-gateway/src/test/java/io/github/cursodsousa/cloudgateway/MsCloudGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.cursodsousa.cloudgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsCloudGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
