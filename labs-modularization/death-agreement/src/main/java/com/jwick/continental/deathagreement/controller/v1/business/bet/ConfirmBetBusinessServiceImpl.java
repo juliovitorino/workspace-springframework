09451614d43f13b4ec0bb8096969955105dbff30
juliovitorino <julio.vitorino@gmail.com>
2023-10-10 16:22:29 -0300

    Adição do procedimento para confirmar o ticket e sua devida classe de teste

---
+++ labs-modularization/death-agreement/src/main/java/com/jwick/continental/deathagreement/controller/v1/business/bet/ConfirmBetBusinessServiceImpl.java
@@ -0,0 +1,38 @@
+package com.jwick.continental.deathagreement.controller.v1.business.bet;
+
+import br.com.jcv.commons.library.commodities.enums.GenericStatusEnums;
+import br.com.jcv.commons.library.commodities.service.BusinessService;
+import com.jwick.continental.deathagreement.dto.BetDTO;
+import com.jwick.continental.deathagreement.dto.BetObjectDTO;
+import com.jwick.continental.deathagreement.exception.BetNotFoundException;
+import com.jwick.continental.deathagreement.exception.BetObjectNotFoundException;
+import com.jwick.continental.deathagreement.service.AbstractContinentalServices;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
+
+@Service
+@Slf4j
+public class ConfirmBetBusinessServiceImpl extends AbstractContinentalServices implements ConfirmBetBusinessService {
+    @Override
+    @Transactional(transactionManager="transactionManager",
+            propagation = Propagation.REQUIRED,
+            rollbackFor = Throwable.class,
+            noRollbackFor = {BetObjectNotFoundException.class, BetNotFoundException.class}
+    )
+    public Boolean execute(UUID processId, UUID ticket) {
+        log.info("execute :: is starting for ticket => {}", ticket);
+        BetDTO betDTO = betService.findBetByTicketAndStatus(ticket, GenericStatusEnums.PENDENTE.getShortValue());
+        BetObjectDTO target = betObjectService.findById(betDTO.getIdBetObject());
+
+        betService.updateStatusById(betDTO.getId(), GenericStatusEnums.ATIVO.getShortValue());
+        target.setJackpot(target.getJackpot() + betDTO.getBet());
+        target.setJackpotPending(target.getJackpotPending() - betDTO.getBet());
+        betObjectService.salvar(target);
+        log.info("execute :: Your ticket => {} has been activated", ticket);
+        return true;
+    }
+}
