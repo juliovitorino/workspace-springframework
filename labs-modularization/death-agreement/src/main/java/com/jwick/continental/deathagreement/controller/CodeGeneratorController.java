e74d64c9e3bba91ce145aa916a5f3e4e3cb41498
juliovitorino <julio.vitorino@gmail.com>
2023-10-12 18:46:31 -0300

    fix de code smell apontado pelo sonarqube

--- labs-modularization/death-agreement/src/main/java/com/jwick/continental/deathagreement/controller/CodeGeneratorController.java
+++ labs-modularization/death-agreement/src/main/java/com/jwick/continental/deathagreement/controller/CodeGeneratorController.java
@@ -33,8 +33,8 @@ public class CodeGeneratorController {
             List<WritableCode> userCodes = generatorMainStream.generate(User.class);
             generatorMainStream.flushCode(userCodes);
             */
-            List<WritableCode> BetCodes = generatorMainStream.generate(Bet.class);
-            generatorMainStream.flushCode(BetCodes);
+            List<WritableCode> betCodes = generatorMainStream.generate(Bet.class);
+            generatorMainStream.flushCode(betCodes);
             return ResponseEntity.ok().build();
         } catch(CodeGeneratorFolderStructureNotFound e) {
             return ResponseEntity.badRequest().build();
