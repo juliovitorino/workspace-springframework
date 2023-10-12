7f1b2e61eac909d32952c16701bdabf2969c7f8e
juliovitorino <julio.vitorino@gmail.com>
2023-10-12 18:15:19 -0300

    Validação de propriedades anotadas por regex dentro do DTO

---
+++ jcv-modularization/jcv-commons-library/src/main/java/br/com/jcv/commons/library/utility/RegexValidator.java
@@ -0,0 +1,36 @@
+package br.com.jcv.commons.library.utility;
+
+import br.com.jcv.commons.library.commodities.annotation.RegexValidation;
+import br.com.jcv.commons.library.commodities.exception.InvalidRegexException;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+@Component
+public class RegexValidator {
+
+    private RegexValidator() {}
+
+    public static <T> Boolean execute(Class<T> inspectClass, String field, String content) {
+        try {
+            RegexValidation exp = inspectClass.getDeclaredField(field).getAnnotation(RegexValidation.class);
+            if(execute(exp.regex(), content)) {
+                return true;
+            } else {
+                throw new InvalidRegexException("Content " + field + " does not matches with annotation regex " + exp.regex(),
+                        HttpStatus.BAD_REQUEST);
+            }
+        } catch (NoSuchFieldException e) {
+            throw new InvalidRegexException("Field " + field + " does not exist ",
+                    HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    private static Boolean execute(String regex, String content) {
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(content);
+        return matcher.matches();
+    }
+}
