0af5c93e41640410ae27e5d35743c3414b0da881
juliovitorino <julio.vitorino@gmail.com>
2023-10-16 08:59:11 -0300

    Adição da funcionalidade para verificar e negar a confirmação de aposta quando a deathdate está vencida

--- labs-modularization/death-agreement/src/main/java/com/jwick/continental/deathagreement/controller/v1/business/bet/ConfirmBetBusinessServiceImpl.java
+++ labs-modularization/death-agreement/src/main/java/com/jwick/continental/deathagreement/controller/v1/business/bet/ConfirmBetBusinessServiceImpl.java
@@ -1,12 +1,16 @@
 package com.jwick.continental.deathagreement.controller.v1.business.bet;
 
 import br.com.jcv.commons.library.commodities.enums.GenericStatusEnums;
+import br.com.jcv.commons.library.commodities.exception.CommoditieBaseException;
+import br.com.jcv.commons.library.utility.DateUtility;
 import com.jwick.continental.deathagreement.dto.BetDTO;
 import com.jwick.continental.deathagreement.dto.BetObjectDTO;
 import com.jwick.continental.deathagreement.exception.BetNotFoundException;
 import com.jwick.continental.deathagreement.exception.BetObjectNotFoundException;
+import com.jwick.continental.deathagreement.exception.PendingDeathDateDueException;
 import com.jwick.continental.deathagreement.service.AbstractContinentalServices;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
@@ -25,6 +29,10 @@ public class ConfirmBetBusinessServiceImpl extends AbstractContinentalServices i
     public Boolean execute(UUID processId, UUID ticket) {
         log.info("execute :: is starting for ticket => {}", ticket);
         BetDTO betDTO = betService.findBetByTicketAndStatus(ticket, GenericStatusEnums.PENDENTE.getShortValue());
+        log.info("execute :: checking if pending death date {} is due",betDTO.getDeathDate().toString());
+        if(DateUtility.compare(dateTime.getToday(), DateUtility.getDate(betDTO.getDeathDate())) == -1) {
+            throw new PendingDeathDateDueException("Pending death date is due", HttpStatus.BAD_REQUEST);
+        }
         BetObjectDTO target = betObjectService.findById(betDTO.getIdBetObject());
 
         betService.updateStatusById(betDTO.getId(), GenericStatusEnums.ATIVO.getShortValue());
