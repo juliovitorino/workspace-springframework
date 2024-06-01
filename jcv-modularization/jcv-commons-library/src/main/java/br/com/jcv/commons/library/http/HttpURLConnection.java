c324c8cd47469fd126576216e37a4b90142a65b1
julio.vitorino <julio.vitorino@accountfy.com>
2024-06-01 17:20:37 -0300

    improving for HttpURLConnetion class using POST verb and Header parameters

--- jcv-modularization/jcv-commons-library/src/main/java/br/com/jcv/commons/library/http/HttpURLConnection.java
+++ jcv-modularization/jcv-commons-library/src/main/java/br/com/jcv/commons/library/http/HttpURLConnection.java
@@ -3,10 +3,19 @@ package br.com.jcv.commons.library.http;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.URL;
+import java.util.Map;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import br.com.jcv.commons.library.commodities.exception.CommoditieBaseException;
 import lombok.extern.slf4j.Slf4j;
 
 @Component
@@ -36,7 +45,95 @@ public class HttpURLConnection {
 
 			return response.toString();
 		} else {
-			return "Algo deu errado na request direta";
+			return "Something went wrong with request";
+		}
+
+	}
+
+	public <T,C> C sendPOST(String url, T body, Class<C> typeToConvert) throws IOException, CommoditieBaseException {
+		URL obj = new URL(url);
+		log.info("POST URL :: {}", url);
+
+		java.net.HttpURLConnection con = (java.net.HttpURLConnection) obj.openConnection();
+		con.setRequestMethod("POST");
+		con.setRequestProperty("User-Agent", "Mozilla/5.0");
+		con.setRequestProperty("Content-Type", "application/json");
+
+		// For POST only - START
+		con.setDoOutput(true);
+		OutputStream os = con.getOutputStream();
+		os.write(new ObjectMapper().writeValueAsString(body).getBytes());
+		os.flush();
+		os.close();
+		// For POST only - END
+
+		int responseCode = con.getResponseCode();
+		log.info("POST Response Code :: {}", responseCode);
+
+		if (responseCode == java.net.HttpURLConnection.HTTP_OK) {
+			BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+			String inputLine;
+			StringBuilder response = new StringBuilder();
+
+			while ((inputLine = in.readLine()) != null) {
+				response.append(inputLine);
+			}
+			in.close();
+
+			C returnTo;
+			try {
+				returnTo = new ObjectMapper().readValue(response.toString(), typeToConvert);
+			} catch (JsonProcessingException e) {
+				returnTo = (C) response.toString();
+			}
+			return returnTo;
+		} else {
+			throw new CommoditieBaseException("Something went wrong with request", HttpStatus.BAD_GATEWAY);
+		}
+
+	}
+
+	public <T,C> C sendPOST(String url, Map<String,String> header, T body, Class<C> typeToConvert) throws IOException, CommoditieBaseException {
+		URL obj = new URL(url);
+		log.info("POST URL :: {}", url);
+
+		java.net.HttpURLConnection con = (java.net.HttpURLConnection) obj.openConnection();
+		con.setRequestMethod("POST");
+		con.setRequestProperty("User-Agent", "Mozilla/5.0");
+		con.setRequestProperty("Content-Type", "application/json");
+
+		header.forEach(con::setRequestProperty);
+
+		// For POST only - START
+		con.setDoOutput(true);
+		OutputStream os = con.getOutputStream();
+		os.write(new ObjectMapper().writeValueAsString(body).getBytes());
+		os.flush();
+		os.close();
+		// For POST only - END
+
+		int responseCode = con.getResponseCode();
+		log.info("POST Response Code :: {}", responseCode);
+
+		if (responseCode == java.net.HttpURLConnection.HTTP_OK) {
+			BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+			String inputLine;
+			StringBuilder response = new StringBuilder();
+
+			while ((inputLine = in.readLine()) != null) {
+				response.append(inputLine);
+			}
+			in.close();
+
+			C returnTo;
+			try {
+				returnTo = new ObjectMapper().readValue(response.toString(), typeToConvert);
+			} catch (JsonProcessingException e) {
+				returnTo = (C) response.toString();
+			}
+			return returnTo;
+		} else {
+			throw new CommoditieBaseException("Something went wrong with request", HttpStatus.BAD_GATEWAY);
 		}
 
 	}
